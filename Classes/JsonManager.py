48b28baa3c5aae7601f11ce662a1eee9aa156bae
Jordan-Bazin <jordan.b35@hotmail.fr>
2023-01-24 00:06:23 +0100

    ca bouge

--- Classes/JsonManager.py
+++ Classes/JsonManager.py
@@ -6,12 +6,21 @@ class JsonManager:
         self.data = self.load()
         
     def load(self):
-        with open(self.path, 'r') as f:
-            return json.load(f)
+        try:
+            fichier = open(self.path, "r")
+            return json.load(fichier)
+        except FileNotFoundError:
+            print("le fichier n'existe pas")
+            exit(1)
         
-    def save(self):
-        with open(self.path, 'w') as f:
-            json.dump(self.data, f, indent=4)
+        
+    def save(self, data):
+        try:
+            fichier = open(self.path, "w")
+            json.dump(self.data, fichier, indent=4)
+        except FileNotFoundError:
+            print("le fichier n'existe pas")
+            exit(1)
             
     def getData(self):
         return self.data
