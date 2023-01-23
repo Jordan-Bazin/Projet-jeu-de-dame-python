20d5de70a6124d1e5d8b6d8ece09ab5327e9c5d2
Yuiiitsu <99472078+Yuiiitsu@users.noreply.github.com>
2023-01-23 16:55:57 +0100

    Menu

--- menu.py
+++ menu.py
@@ -5,12 +5,20 @@ from Classes.Plateau import Plateau
 
 #Création d'un menu
 def menu():
+    
+    print("Bienvenue dans le jeu de dame")
+    
     print("Tapez 1 pour lancer une nouvelle partie")
     print("Tapez 2 pour lancer une ancienne partie")
     print("Tapez 3 pour envoyer le tableau des scores par mail")
     print("Tapez 4 pour quitter le jeu")
     choix = input("Votre choix : ")
-
+    match choix:
+        case "1":
+            get_new_game()
+        case "2":
+            get_old_game()
+        case "3":
 
 def get_new_game():
     print("Nouvelle partie")
