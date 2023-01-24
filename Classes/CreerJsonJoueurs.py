97d8860cc1a9fbd0b50a7dbcdb116b956651e5fb
natha <nathan.gouarderes@hotmail.fr>
2023-01-24 10:41:18 +0100

    test

--- Classes/CreerJsonJoueurs.py
+++ Classes/CreerJsonJoueurs.py
@@ -1,1 +1,21 @@
-jsonJoueurs = open("TableauJoueurs.json", "r")
+import json
+jsonJoueurs = {
+   "Joueur":[
+      {
+         "Nom":"Jordan",
+         "NombreVictoires":0
+      },
+      {
+         "Nom":"Thomas",
+         "NombreVictoires":0
+      },
+      {
+            "Nom":"Alexandre",
+            "NombreVictoires":0
+      }
+   ]
+}
+
+
+jsonOuverture = open("TableauJoueurs.json", "w")
+json.dump(jsonJoueurs, jsonOuverture, indent=3)
