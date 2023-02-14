e792e06320c3da8b23d2262bfc033d7eb9d11890
Jordan-Bazin <jordan.b35@hotmail.fr>
2023-02-14 14:54:04 +0100

    fez

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from Classes.Plateau import Plateau
 from Classes.Joueur import Joueur
-import copy
 import menu
 
 if __name__ == '__main__':
     menu.menu()
 
-    # ajouter joueur1 et joueur2 en tant que propriétés de plateau
     
